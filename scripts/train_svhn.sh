d626b977894fb2c365a3664af287c831e431d158
Eric Chen <deeplearningericchen@gmail.com>
2024-04-09 09:56:35 -0700

    big run

--- scripts/train_svhn.sh
+++ scripts/train_svhn.sh
@@ -3,13 +3,13 @@ poetry run python3 train_svhn.py \
     --model_config=../configs/experiment.json \
     --image_size=64 \
     --batch_size=64 \
-    --num_epochs=200 \
-    --lr=3e-4 \
-    --weight_decay=0.0000 \
+    --num_epochs=500 \
+    --lr=1e-3 \
+    --weight_decay=0.0001 \
     --data_augment \
-    --augment_prob=0.00 \
+    --augment_prob=0.05 \
     --eval_every=20 \
     --checkpoint_epoch=10 \
-    --save_dir=../checkpoints/goodies \
+    --save_dir=../checkpoints/awesome \
     # --save_dir=../checkpoints/svhn_experiment_small_wd \
     # --save_dir=../checkpoints/svhn_experiment_big \
